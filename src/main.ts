e6b0258bc51c1a1ffd0471981868a07f0d571a21
PARK JUHEE <atig00328@uangel.com>
2023-10-30 17:19:06 +0900

    test

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookiePaser from 'cookie-parser';
+import * as session from 'express-session';
+import * as passport from 'passport';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookiePaser());
+  app.use(
+      session({
+        secret: 'very-important-secret',
+        resave: false,
+
+        saveUninitialized: false,
+        cookie: { maxAge: 3600000},
+      }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  await app.listen(3000);
+}
+bootstrap();
